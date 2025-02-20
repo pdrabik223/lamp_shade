1161b00b4277bf984e7b51a4f97d001f42071b65
pdrabik223 <pdrabik223@gmail.com>
2025-02-21 00:48:36 +0100

    we happy

--- README.md
+++ README.md
@@ -1,15 +1,34 @@
 # lamp_shade
 
+Very simple animated desk lamp based on RPI Pico, that was made in a hurry... 
+
+Prepared animations include (in order):
+1. simply_on - lamp shines the brightest light possible
+2. simply_off - light is turned off
+3. breeze - slowly oscillates between dim and bright light 
+4. candle - emulates flame of a candle
+
+
+# Set up guide
+
+1. Install RPI Pico SDK - one approach is to use [Thonny](https://thonny.org/)
+2. Flash your board with  MircroPython, it can be downloaded from official site[MircoPython download](https://www.raspberrypi.com/documentation/microcontrollers/micropython.html), or one can use version [attached in repository](./RPI_PICO-20241129-v1.24.1.uf2)
+3. Connect to pico and run [main.py](./src/main.py) file
+4. Once your scrip is running successfully, flash main.py file to the board using Thonny 
+
+That's it!
+
+
+
+
 [3d models](https://a360.co/41rl0oj)
 
-
-[MircoPython download](https://www.raspberrypi.com/documentation/microcontrollers/micropython.html)
- 
 
 
 Problems:
 1. LED's are shit, so the corelation between PWM duty cycle and brightness is not linear, I need to create function that takes that into account
 2. Timing is not precise (I think) this should be investigated
-3. We need proto-threads for the button to feel good
-4. Remodeled cover would be nice    
-
+3. We need proto-threads for the button to feel good - Done
+4. Remodeled cover would be nice - Done Requires printing
+5. Remember last animation [en example](https://electrocredible.com/rpi-pico-save-data-permanently-flash-micropython/) - Unnecessary if we have animation "OFF", PRI simply newer will be turned off 
+6. Flash program to permanent storage 
--- src/main.py
+++ src/main.py
@@ -1,98 +1,190 @@
 from machine import Pin,  PWM 
 import utime
 import random
+import _thread
+import gc
 
-
-
-
+BUTTON = Pin(15, Pin.IN,Pin.PULL_UP)
 PWM = PWM(Pin(12))
 PWM.freq(1000)
 current_duty_cycle = 0
 
-def set_duty_cycle(duty_cycle):
-    current_duty_cycle = duty_cycle
-    PWM.duty_u16(duty_cycle)
-    
-
-BUTTON = Pin(15, Pin.IN,Pin.PULL_UP)
-
+stop_animation = False
+animation_index = 0
 animations = []
 
+
+def set_brightness(duty_cycle, sleep_time_s)->bool:
+    set_duty_cycle(duty_cycle)
+    return delay(sleep_time_s)
+
+
+def set_duty_cycle(duty_cycle):
+    global current_duty_cycle
+    if duty_cycle > 65535:
+        raise ValueError(f"Duty cycle must be less than 65535, current: {duty_cycle}")
+    if duty_cycle < 0:
+        raise ValueError(f"Duty cycle must be bigger than 0, current: {duty_cycle}")
+    current_duty_cycle = duty_cycle
+    PWM.duty_u16(duty_cycle)
+
+def delay(sleep_time_s) -> bool:
+    global stop_animation
+    if (sleep_time_s < 0.1):
+        utime.sleep(sleep_time_s)
+        return stop_animation
+    
+    intervals = int(sleep_time_s / 0.1)
+    for i in range(intervals):
+        utime.sleep(0.1)
+        if stop_animation:
+            return True
+    utime.sleep(sleep_time_s - (intervals * 0.1))
+    
+    return stop_animation
 
 
 def animation(f):
     animations.append(f)
     return f
 
-# @animation
-# def led_animation_on(animation_duration_s = 2):
-  
-#     set_duty_cycle(65025)
-#     utime.sleep(animation_duration_s)
-# @animation
-# def led_animation_off(animation_duration_s = 2):
-  
-#     set_duty_cycle(0)
-#     utime.sleep(animation_duration_s)
 
 @animation
-def breath(min_brightness= 0.3, max_brightness = 0.9, animation_duration_s = 20):
-    
-    min_duty_cycle = int(65025 * min_brightness)
-    max_duty_cycle = int(65025 * max_brightness)
-    
-    sleep_time = (animation_duration_s / (max_duty_cycle - min_duty_cycle)) / 2
+def simply_on():
+  
+    set_duty_cycle(65535)
+    delay(10)
 
-    for duty in range(min_duty_cycle, max_duty_cycle, 1):
-        set_duty_cycle(duty)
-        utime.sleep(sleep_time)
-        
-    for duty in range(max_duty_cycle, min_duty_cycle, -1):
-        set_duty_cycle(duty)
-        utime.sleep(sleep_time)
-        
-# @animation
-# def fire(min_brightness = 0.2, max_brightness = 0.9, min_animation_duration_s = 0.5, max_animation_duration_s = 6):
+def gradient_color_shift(animation_steps:List[int], animation_duration_s:float):
+     for duty in animation_steps:
+        if set_brightness(duty, animation_duration_s / len(animation_steps)):
+            return
+
+@animation
+def breeze(min_brightness= 0.3, max_brightness = 0.9, animation_duration_s = 10):
     
+    min_duty_cycle = int(65535 * min_brightness)
+    max_duty_cycle = int(65535 * max_brightness)
     
+    animation_duration_s /= 2 # animation is divided into two parts  
+   
+    animation_steps = range(min_duty_cycle, max_duty_cycle, 10)
+    gradient_color_shift(animation_steps, animation_duration_s)
     
-    
-#     max_duty_cycle = int(65025 * max_brightness) - (current_duty_cycle//4)
-#     min_duty_cycle = int(65025 * min_brightness) + (current_duty_cycle//4)
-    
-#     offset = random.randint(min_duty_cycle, max_duty_cycle)
-    
-#     next_duty_cycle = current_duty_cycle + offset
-    
-#     sleep_time = random.random() * (max_animation_duration_s - min_animation_duration_s) + min_animation_duration_s
-    
-#     if current_duty_cycle < next_duty_cycle:
-#         sleep_time = sleep_time / (next_duty_cycle - current_duty_cycle)
-#         for duty in range(current_duty_cycle, next_duty_cycle, 1):
-#             set_duty_cycle(duty)
-#             utime.sleep(sleep_time)
-#     else:
-#         sleep_time = sleep_time / (current_duty_cycle - next_duty_cycle)
-#         for duty in range(next_duty_cycle, current_duty_cycle, -1):
-#             set_duty_cycle(duty)
-#             utime.sleep(sleep_time)
-        
-    
-    
+    animation_steps = range(max_duty_cycle, min_duty_cycle, -10)
+    gradient_color_shift(animation_steps, animation_duration_s)
 
 
+def get_random_small_float():
+    return random.uniform(-0.1, 0.1)
 
-def main():
-    animation_index = 0
+def quick_bling():
+    # global current_duty_cycle
+    
+    min_duty_cycle = int(65535 * (0.3 + get_random_small_float()))
+    max_duty_cycle = int(65535 * (0.8 + get_random_small_float()))
+    
+    animation_steps = range(min_duty_cycle, max_duty_cycle, random.randint(20,30))
+    gradient_color_shift(animation_steps, 0)
+
+def stable_oscillation():
+    min_duty_cycle = int(65535 * (0.4+ get_random_small_float()))
+    max_duty_cycle = int(65535 * (0.8+ get_random_small_float()))
+    animation_steps = range(min_duty_cycle, max_duty_cycle, random.randint(5,10))
+    gradient_color_shift(animation_steps, 0.1)
+
+    animation_steps = range(max_duty_cycle, min_duty_cycle, -1*random.randint(5,10))
+    gradient_color_shift(animation_steps, 0.1)
+
+
+
+def stabilization():
+    min_duty_cycle = int(65535 * (0.4+ get_random_small_float()))
+    max_duty_cycle = int(65535 * (0.7+ get_random_small_float()))
+    animation_steps = range(min_duty_cycle, max_duty_cycle, random.randint(2,8))
+    gradient_color_shift(animation_steps, 2 + (get_random_small_float()*2))
+    
+    animation_steps = range(max_duty_cycle, min_duty_cycle, -1*random.randint(2,8))
+    gradient_color_shift(animation_steps, 2 + (get_random_small_float()*2))
+
+def stabilization2(offset = 0):
+    min_duty_cycle = int(65535 * (0.6 + offset))
+    max_duty_cycle = int(65535 * (0.9 + offset))
+    animation_steps = range(min_duty_cycle, max_duty_cycle, random.randint(2,8))
+    gradient_color_shift(animation_steps, 3)
+    
+    animation_steps = range(max_duty_cycle, min_duty_cycle, -1*random.randint(2,8))
+    gradient_color_shift(animation_steps, 3)
+
+    
+@animation
+def small_candle(min_brightness = 0.2, max_brightness = 0.9, min_animation_duration_s = 0.5, max_animation_duration_s = 6):
+   
+    for i in range( random.randint(4,7)):
+        stabilization()
+   
+    for i in range( random.randint(4,8)):
+        quick_bling()
+
+  
+@animation
+def big_candle(min_brightness = 0.2, max_brightness = 0.9, min_animation_duration_s = 0.5, max_animation_duration_s = 6):
+   
+    for i in range( random.randint(4,7)):
+        stabilization()
+   
+    for i in range( random.randint(4,8)):
+        quick_bling()
+    
+    stabilization()
+    
+    if random.random() > 0.5:
+        for i in range( random.randint(8,16)):
+            stabilization2()
+    else:
+        for i in range( random.randint(8,16)):
+            stabilization2(-0.2)
+            
+               
+@animation
+def simply_off():
+  
+    set_duty_cycle(0)
+    delay(10)
+     
+def button_listener(sleep_time = 0.1):
+    global animation_index
+    global stop_animation
+    global animations
+    
     while(True):
-        
         if BUTTON.value() == 0:
+            stop_animation = True
             animation_index += 1
             if len(animations) == animation_index:
                 animation_index = 0
+            utime.sleep(sleep_time*2)
             
+        utime.sleep(sleep_time)    
+
+
+def main():
+    global animation_index
+    global stop_animation
+    global animations
+    
+    _thread.start_new_thread(button_listener, ())
+    
+    while(True):
+        if stop_animation:
+            stop_animation = False
+            set_duty_cycle(0)
+            utime.sleep(0.2)
+
         animations[animation_index]()
-        
-main()
+        gc.collect()
+    
+if __name__ == "__main__":
+    main()
     
     
