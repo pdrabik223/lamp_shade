e00c8ae4190d56ff3dc1457dc7cab82a15fc019a
pdrabik223 <pdrabik223@gmail.com>
2025-02-21 10:54:57 +0100

    refactor main.py: improve code readability and optimize functions

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ stop_animation = False
 animation_index = 0
 animations = []
 
+
 def set_brightness(duty_cycle, sleep_time_s) -> bool:
     set_duty_cycle(duty_cycle)
     return delay(sleep_time_s)
@@ -32,7 +33,7 @@ def delay(sleep_time_s) -> bool:
         return stop_animation
 
     intervals = int(sleep_time_s / 0.1)
-    for i in range(intervals):
+    for _ in range(intervals):
         utime.sleep(0.1)
         if stop_animation:
             return True
@@ -41,21 +42,22 @@ def delay(sleep_time_s) -> bool:
     return stop_animation
 
 
+def gradient_color_shift(animation_steps, animation_duration_s):
+    for duty in animation_steps:
+        if set_brightness(duty, animation_duration_s / len(animation_steps)):
+            return
+
+
 def animation(f):
     animations.append(f)
     return f
+
 
 @animation
 def simply_on():
 
     set_duty_cycle(65535)
     delay(10)
-
-
-def gradient_color_shift(animation_steps, animation_duration_s):
-    for duty in animation_steps:
-        if set_brightness(duty, animation_duration_s / len(animation_steps)):
-            return
 
 
 @animation
@@ -73,35 +75,37 @@ def breeze(min_brightness=0.3, max_brightness=0.9, animation_duration_s=10):
     gradient_color_shift(animation_steps, animation_duration_s)
 
 
-def get_random_small_float():
-    return random.uniform(-0.1, 0.1)
+def get_random_small_float(offset=0):
+    return random.uniform(-0.1, 0.1) + offset
 
 
 def quick_bling():
-    min_duty_cycle = int(65535 * (0.3 + get_random_small_float()))
-    max_duty_cycle = int(65535 * (0.8 + get_random_small_float()))
+    min_duty_cycle = int(65535 * get_random_small_float(0.3))
+    max_duty_cycle = int(65535 * get_random_small_float(0.8))
 
     animation_steps = range(min_duty_cycle, max_duty_cycle, random.randint(20, 30))
     gradient_color_shift(animation_steps, 0)
 
 
 def stable_oscillation():
-    min_duty_cycle = int(65535 * (0.4 + get_random_small_float()))
-    max_duty_cycle = int(65535 * (0.7 + get_random_small_float()))
+    min_duty_cycle = int(65535 * get_random_small_float(0.4))
+    max_duty_cycle = int(65535 * get_random_small_float(0.7))
+
     animation_steps = range(min_duty_cycle, max_duty_cycle, random.randint(2, 8))
     gradient_color_shift(animation_steps, 2 + (get_random_small_float() * 2))
 
-    animation_steps = range(max_duty_cycle, min_duty_cycle, -1 * random.randint(2, 8))
+    animation_steps = range(max_duty_cycle, min_duty_cycle, random.randint(-8, -2))
     gradient_color_shift(animation_steps, 2 + (get_random_small_float() * 2))
 
 
 def stabilization(offset=0):
     min_duty_cycle = int(65535 * (0.6 + offset))
     max_duty_cycle = int(65535 * (0.9 + offset))
+
     animation_steps = range(min_duty_cycle, max_duty_cycle, random.randint(2, 8))
     gradient_color_shift(animation_steps, 3)
 
-    animation_steps = range(max_duty_cycle, min_duty_cycle, -1 * random.randint(2, 8))
+    animation_steps = range(max_duty_cycle, min_duty_cycle, random.randint(-8, -2))
     gradient_color_shift(animation_steps, 3)
 
 
@@ -109,36 +113,39 @@ def stabilization(offset=0):
 def small_candle():
 
     for _ in range(random.randint(4, 7)):
-        stable_oscillation()
+        stable_oscillation()  # candle is "flowing" from left to right in a semi stable way
 
     for _ in range(random.randint(4, 8)):
-        quick_bling()
+        quick_bling()  # flame is flickering quickly and dynamically
 
 
 @animation
 def big_candle():
 
     for _ in range(random.randint(4, 7)):
-        stable_oscillation()
+        stable_oscillation()  # candle is "flowing" from left to right in a semi stable way
 
     for _ in range(random.randint(4, 8)):
-        quick_bling()
+        quick_bling()  # flame is flickering quickly and dynamically
 
     for _ in range(random.randint(4, 7)):
-        stable_oscillation()
+        stable_oscillation()  # back to stable oscillation
 
     if random.random() > 0.5:
         for _ in range(random.randint(8, 16)):
-            stabilization()
+            stabilization()  # candle moves slowly is in stable state
     else:
         for _ in range(random.randint(8, 16)):
-            stabilization(-0.2)
+            stabilization(
+                -0.2
+            )  # candle moves slowly is in stable state, but is dimmer overall
 
 
 @animation
 def simply_off():
     set_duty_cycle(0)
     delay(10)
+
 
 def button_listener(sleep_time=0.1):
     global animation_index
